4731117ca840614847383e9ef6dadfcb37c717e8
lorennn <roy830817@yahoo.com.tw>
2022-01-24 12:52:10 +0800

    20221252

--- common/common.js
+++ common/common.js
@@ -96,18 +96,23 @@ jQuery(document).ready(function () {
     // 測試用
     if(window.DeviceOrientationEvent) {
         window.addEventListener('deviceorientation', function(event) {
-        var a = document.getElementById('alpha'),
-            b = document.getElementById('beta'),
-            g = document.getElementById('gamma'),
-            alpha = event.alpha,
-            beta = event.beta,
-            gamma = event.gamma;
+            // 獲取三軸移動數值
+            var a = document.getElementById('alpha'),
+                b = document.getElementById('beta'),
+                g = document.getElementById('gamma'),
+                alpha = event.alpha,
+                beta = event.beta,
+                gamma = event.gamma;
+        
+            a.innerHTML = Math.round(alpha);
+            b.innerHTML = Math.round(beta);
+            g.innerHTML = Math.round(gamma);
+
+            // 獲取時間
+            var curTime = new Date().getTime();
+            alert("curTime00,",curTime);
       
-        a.innerHTML = Math.round(alpha);
-        b.innerHTML = Math.round(beta);
-        g.innerHTML = Math.round(gamma);
-      
-          }, false);
+        }, false);
       }else{
           document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
       }
